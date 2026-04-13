fd491d94d9c8b7f1a67b4ff5e0e4051cb8e0da27
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-04-13 23:03:17 +0530

    fix: install each chocolatey package with '-y' flag individually

--- windows/packages.ps1
+++ windows/packages.ps1
@@ -2,24 +2,24 @@
 # Install all packages via Chocolatey
 # Run as Administrator
 
-choco install -y `
-    brave `
-    cmake `
-    fzf `
-    git `
-    github-desktop `
-    googlechrome `
-    make `
-    mingw `
-    mysql `
-    neovim `
-    nodejs `
-    oh-my-posh `
-    openjdk `
-    powershell-core `
-    python `
-    ripgrep `
-    vscode `
-    zig
+choco install -y 
+choco install brave  -y
+choco install cmake  -y
+choco install fzf  -y
+choco install git  -y
+choco install github-desktop  -y
+choco install googlechrome  -y
+choco install make  -y
+choco install mingw  -y
+choco install mysql  -y
+choco install neovim  -y
+choco install nodejs  -y
+choco install oh-my-posh  -y
+choco install openjdk  -y
+choco install powershell-core  -y
+choco install python  -y
+choco install ripgrep  -y
+choco install vscode  -y
+choco install zig -y
 
 # choco install brave cmake fzf git github-desktop googlechrome make mingw mysql neovim nodejs oh-my-posh openjdk powershell-core python ripgrep vscode zig -y
