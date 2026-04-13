27ce1bf41f46cb5663df9ac1d10c95a9478aee88
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-04-13 21:29:35 +0530

    chore: add script for installing chocolatey and other packages

---
+++ windows/packages.ps1
@@ -0,0 +1,25 @@
+# packages.ps1
+# Install all packages via Chocolatey
+# Run as Administrator
+
+choco install -y `
+    brave `
+    cmake `
+    fzf `
+    git `
+    github-desktop `
+    googlechrome `
+    make `
+    mingw `
+    mysql `
+    neovim `
+    nodejs `
+    oh-my-posh `
+    openjdk `
+    powershell-core `
+    python `
+    ripgrep `
+    vscode `
+    zig
+
+# choco install brave cmake fzf git github-desktop googlechrome make mingw mysql neovim nodejs oh-my-posh openjdk powershell-core python ripgrep vscode zig -y
