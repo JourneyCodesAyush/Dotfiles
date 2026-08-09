56359b9631cb1df2918bd85cc37e29fcd56478d4
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-08-09 20:38:56 +0530

    refactor(windows): use packages.config for package installation

--- windows/choco-packages.ps1
+++ windows/choco-packages.ps1
@@ -2,27 +2,4 @@
 # Install all packages via Chocolatey
 # Run as Administrator
 
-choco install -y 
-choco install brave  -y
-choco install cmake  -y
-choco install fzf  -y
-choco install fd -y
-choco install git  -y
-choco install github-desktop  -y
-choco install googlechrome  -y
-choco install make  -y
-choco install maven  -y
-choco install mingw  -y
-choco install mysql  -y
-choco install neovim  -y
-choco install nodejs  -y
-choco install oh-my-posh  -y
-choco install openjdk  -y
-choco install powershell-core  -y
-choco install python  -y
-choco install ripgrep  -y
-choco install vlc -y
-choco install vscode  -y
-choco install zig -y
-
-# choco install brave cmake fzf fd git github-desktop googlechrome make maven mingw mysql neovim nodejs oh-my-posh openjdk powershell-core python ripgrep vlc vscode zig -y
+choco install "$PSScriptRoot\..\packages.config" -y
